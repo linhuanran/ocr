03c4e6a98af0e4947710d613c038dffd57048a0b
hope <lucifinil006@gmail.com>
2020-08-15 16:36:15 +0800

    天若5.0源码

---
+++ FmScreenPaste.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TrOCR
+{
+
+	public partial class FmScreenPaste : global::System.Windows.Forms.Form
+	{
+
+		protected override void Dispose(bool disposing)
+		{
+			bool flag = disposing && this.components != null;
+			if (flag)
+			{
+				this.components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		private void InitializeComponent()
+		{
+			this.RightMenu = new global::System.Windows.Forms.ContextMenuStrip();
+			this.关闭ToolStripMenuItem = new global::System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new global::System.Windows.Forms.ToolStripSeparator();
+			this.toolStripMenuItem6 = new global::System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem8 = new global::System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new global::System.Windows.Forms.ToolStripSeparator();
+			this.置顶ToolStripMenuItem = new global::System.Windows.Forms.ToolStripMenuItem();
+			this.RightMenu.SuspendLayout();
+			base.SuspendLayout();
+			this.RightMenu.Items.AddRange(new global::System.Windows.Forms.ToolStripItem[]
+			{
+				this.关闭ToolStripMenuItem,
+				this.toolStripSeparator1,
+				this.toolStripMenuItem6,
+				this.toolStripMenuItem8,
+				this.toolStripSeparator2,
+				this.置顶ToolStripMenuItem
+			});
+			this.RightMenu.Name = "dSkinContextMenuStrip2";
+			this.RightMenu.ShowImageMargin = false;
+			this.RightMenu.Size = new global::System.Drawing.Size(124, 126);
+			this.RightMenu.Opening += new global::System.ComponentModel.CancelEventHandler(this.RightCMS_Opening);
+			this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+			this.关闭ToolStripMenuItem.Size = new global::System.Drawing.Size(123, 22);
+			this.关闭ToolStripMenuItem.Text = "关闭";
+			this.关闭ToolStripMenuItem.Click += new global::System.EventHandler(this.关闭ToolStripMenuItem_Click);
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new global::System.Drawing.Size(120, 6);
+			this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+			this.toolStripMenuItem6.Size = new global::System.Drawing.Size(123, 22);
+			this.toolStripMenuItem6.Text = "复制图像";
+			this.toolStripMenuItem6.Click += new global::System.EventHandler(this.复制toolStripMenuItem_Click);
+			this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+			this.toolStripMenuItem8.Size = new global::System.Drawing.Size(123, 22);
+			this.toolStripMenuItem8.Text = "图像另存为";
+			this.toolStripMenuItem8.Click += new global::System.EventHandler(this.保存toolStripMenuItem_Click);
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new global::System.Drawing.Size(120, 6);
+			this.置顶ToolStripMenuItem.Name = "置顶窗体ToolStripMenuItem";
+			this.置顶ToolStripMenuItem.Size = new global::System.Drawing.Size(123, 22);
+			this.置顶ToolStripMenuItem.Text = "置顶窗体";
+			this.置顶ToolStripMenuItem.Click += new global::System.EventHandler(this.置顶ToolStripMenuItem_Click);
+			this.ContextMenuStrip = this.RightMenu;
+			base.StartPosition = global::System.Windows.Forms.FormStartPosition.Manual;
+			base.TopMost = true;
+			base.ShowInTaskbar = false;
+			this.RightMenu.ResumeLayout(false);
+			base.ResumeLayout(false);
+		}
+
+		private global::System.ComponentModel.IContainer components;
+
+		private global::System.Windows.Forms.ContextMenuStrip RightMenu;
+
+		private global::System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+
+		private global::System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+
+		private global::System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
+
+		private global::System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+
+		private global::System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+
+		private global::System.Windows.Forms.ToolStripMenuItem 置顶ToolStripMenuItem;
+	}
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+## 天若 OCR 文字识别开源版
+> 该项目已停止维护，欢迎使用新版跨平台 OCR 工具，[树洞 OCR 文字识别](https://github.com/AnyListen/tools-ocr)：https://github.com/AnyListen/tools-ocr
+
+天若 OCR 文字识别开源版是基于 @天若幽心  [开源的代码](https://github.com/tianruoyouxin/tianruoocr_last) 进行完善制作而成。
+
+![天若 OCR 文字识别开源版](http://img.luooqi.com/FqgLxwq7qu7_OLBr5HYO_bi7Rpbw)
+
+## 版本更新
+### V5.0.0
+新版本的主要特性：
+1. 对原来的代码结构进行了简单重构；
+2. 移除了 V4.49 版本的提示更新的弹窗；
+3. 实现了新版的程序自动更新。
+
+总体来讲没有带来什么新功能，后期主要对目前的功能代码进行重构；
+然后逐步跟进新版本的功能。
+
+## 后记
+最后，希望有兴趣的可以共同推动开源版本的发展。
+项目地址：https://github.com/AnyListen/tianruoocr_last
+
+## 下载地址
+- 百度网盘：https://pan.baidu.com/s/17T1MR6R7EQ4zvgeokTMFeA
+- Github：https://github.com/AnyListen/tianruoocr_last/releases
